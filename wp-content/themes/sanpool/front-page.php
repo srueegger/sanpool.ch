15c68ae85b892677793da86536022775718f7332
Samuel Rüegger <srueegger@pop-os.localdomain>
2020-01-10 17:57:39 +0100

    some chnages

--- wp-content/themes/sanpool/front-page.php
+++ wp-content/themes/sanpool/front-page.php
@@ -3,8 +3,8 @@ get_header();
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
 <main id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="">
-		<div class="row">
+	<div>
+		<div class="row no-gutters">
 			<div class="col-12">
 				<div class="headerSlider owl-carousel owl-theme">
 					<div class="item">
@@ -55,61 +55,225 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 	<div class="container">
 		<div class="mt-5">
 			<div class="row justify-content-center">
-				<div class="col-12 col-lg-3">
+				<div class="col-12 col-md-6 col-lg-3">
 					<div class="card">
-						<div class="image-container"><img src="https://picsum.photos/1110/655" class="card-img-top" alt="..."></div>
-						<div class="card-body">
-							<h5 class="card-title">Ein Titel</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
+						<div class="image-container">
+							<picture>
+								<img src="https://picsum.photos/255/353" class="card-img-top" alt="...">
+							</picture>
+						</div>
+						<div class="overlay-seen-container">
+							<div class="inner">
+								<h4 class="text-white">Hallo Welt</h4>
+								<div class="text-center mt-3">
+									<span class="fa-stack fa-2x text-white">
+										<i class="fal fa-circle fa-stack-2x"></i>
+										<i class="fas fa-plus fa-stack-1x fa-inverse"></i>
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="overlay-container">
+							<div class="inner">
+								<p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<div href="#" class="text-center mt-3">
+									<a href="#"><span class="fa-stack fa-2x text-white">
+										<i class="fal fa-circle fa-stack-2x"></i>
+										<i class="fas fa-plus fa-stack-1x fa-inverse"></i>
+									</span></a>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-12 col-lg-3">
+				<div class="col-12 col-md-6 col-lg-3">
 					<div class="card">
-						<div class="image-container"><img src="https://picsum.photos/1110/655" class="card-img-top" alt="..."></div>
-						<div class="card-body">
-							<h5 class="card-title">Ein Titel</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
+						<div class="image-container">
+							<picture>
+								<img src="https://picsum.photos/255/353" class="card-img-top" alt="...">
+							</picture>
+						</div>
+						<div class="overlay-seen-container">
+							<div class="inner">
+								<h4 class="text-white">Hallo Welt</h4>
+								<div class="text-center mt-3">
+									<span class="fa-stack fa-2x text-white">
+										<i class="fal fa-circle fa-stack-2x"></i>
+										<i class="fas fa-plus fa-stack-1x fa-inverse"></i>
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="overlay-container">
+							<div class="inner">
+								<p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<div href="#" class="text-center mt-3">
+									<a href="#"><span class="fa-stack fa-2x text-white">
+										<i class="fal fa-circle fa-stack-2x"></i>
+										<i class="fas fa-plus fa-stack-1x fa-inverse"></i>
+									</span></a>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-12 col-lg-3">
+				<div class="col-12 col-md-6 col-lg-3">
 					<div class="card">
-						<div class="image-container"><img src="https://picsum.photos/1110/655" class="card-img-top" alt="..."></div>
-						<div class="card-body">
-							<h5 class="card-title">Ein Titel</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
+						<div class="image-container">
+							<picture>
+								<img src="https://picsum.photos/255/353" class="card-img-top" alt="...">
+							</picture>
+						</div>
+						<div class="overlay-seen-container">
+							<div class="inner">
+								<h4 class="text-white">Hallo Welt</h4>
+								<div class="text-center mt-3">
+									<span class="fa-stack fa-2x text-white">
+										<i class="fal fa-circle fa-stack-2x"></i>
+										<i class="fas fa-plus fa-stack-1x fa-inverse"></i>
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="overlay-container">
+							<div class="inner">
+								<p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<div href="#" class="text-center mt-3">
+									<a href="#"><span class="fa-stack fa-2x text-white">
+										<i class="fal fa-circle fa-stack-2x"></i>
+										<i class="fas fa-plus fa-stack-1x fa-inverse"></i>
+									</span></a>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-12 col-lg-3">
+				<div class="col-12 col-md-6 col-lg-3">
 					<div class="card">
-						<div class="image-container"><img src="https://picsum.photos/1110/655" class="card-img-top" alt="..."></div>
-						<div class="card-body">
-							<h5 class="card-title">Ein Titel</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
+						<div class="image-container">
+							<picture>
+								<img src="https://picsum.photos/255/353" class="card-img-top" alt="...">
+							</picture>
+						</div>
+						<div class="overlay-seen-container">
+							<div class="inner">
+								<h4 class="text-white">Hallo Welt</h4>
+								<div class="text-center mt-3">
+									<span class="fa-stack fa-2x text-white">
+										<i class="fal fa-circle fa-stack-2x"></i>
+										<i class="fas fa-plus fa-stack-1x fa-inverse"></i>
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="overlay-container">
+							<div class="inner">
+								<p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<div href="#" class="text-center mt-3">
+									<a href="#"><span class="fa-stack fa-2x text-white">
+										<i class="fal fa-circle fa-stack-2x"></i>
+										<i class="fas fa-plus fa-stack-1x fa-inverse"></i>
+									</span></a>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="container mt-5">
-		<div class="row justify-content-center">
-			<div class="col-9">
-				<blockquote class="blockquote text-center">
-					<p class="mb-0"><span class="character">"</span>SanPool isch sbeschte wos je hets gits!</p>
-					<footer class="blockquote-footer text-right">Samuel Rüegger <cite title="Source Title">Web-Entwickler</cite></footer>
-				</blockquote>
+	<div class="container-fluid py-5 mt-5 bg-primary">
+		<div class="row">
+			<div class="col-12 my-4 text-center">
+				<h2 class="h1 text-white">Titel? Ich würde hier nicht das selbe wie bei der Rega nehmen</h2>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col-6 col-md-4 col-lg-2">
+				<div class="shortlink-item text-center">
+					<div class="iconbox">
+						<span class="fa-stack fa-4x">
+							<i class="fal fa-square fa-stack-2x text-white"></i>
+							<i class="far fa-chalkboard-teacher fa-stack-1x text-white"></i>
+						</span>
+					</div>
+					<div class="textbox mt-3">
+						<h5>Instruktor werden</h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-md-4 col-lg-2">
+				<div class="shortlink-item text-center">
+					<div class="iconbox">
+						<span class="fa-stack fa-4x">
+							<i class="fal fa-square fa-stack-2x text-white"></i>
+							<i class="far fa-shopping-cart fa-stack-1x text-white"></i>
+						</span>
+					</div>
+					<div class="textbox mt-3">
+						<h5>SanPool Shop</h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-md-4 col-lg-2">
+				<div class="shortlink-item text-center">
+					<div class="iconbox">
+						<span class="fa-stack fa-4x">
+							<i class="fal fa-square fa-stack-2x text-white"></i>
+							<i class="far fa-id-card fa-stack-1x text-white"></i>
+						</span>
+					</div>
+					<div class="textbox mt-3">
+						<h5>Ersatzausweis</h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-md-4 col-lg-2">
+				<div class="shortlink-item text-center">
+					<div class="iconbox">
+						<span class="fa-stack fa-4x">
+							<i class="fal fa-square fa-stack-2x text-white"></i>
+							<i class="far fa-check fa-stack-1x text-white"></i>
+						</span>
+					</div>
+					<div class="textbox mt-3">
+						<h5>Akkreditierung</h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-md-4 col-lg-2">
+				<div class="shortlink-item text-center">
+					<div class="iconbox">
+						<span class="fa-stack fa-4x">
+							<i class="fal fa-square fa-stack-2x text-white"></i>
+							<i class="far fa-mailbox fa-stack-1x text-white"></i>
+						</span>
+					</div>
+					<div class="textbox mt-3">
+						<h5>Kontakt</h5>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-md-4 col-lg-2">
+				<div class="shortlink-item text-center">
+					<div class="iconbox">
+						<span class="fa-stack fa-4x">
+							<i class="fal fa-square fa-stack-2x text-white"></i>
+							<i class="far fa-terminal fa-stack-1x text-white"></i>
+						</span>
+					</div>
+					<div class="textbox mt-3">
+						<h5>Instruktor werden</h5>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 	<div class="container mt-5">
 		<div class="row">
+			<div class="col-12 my-4">
+				<h2 class="h1">Nächste Kurse</h2>
+			</div>
 			<div class="col-12">
 				<div class="courseSlider owl-carousel owl-theme">
 					<div class="item">
