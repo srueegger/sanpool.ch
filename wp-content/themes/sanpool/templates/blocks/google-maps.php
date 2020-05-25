bbcbfbfcef060901c2392e263385a317267054bf
Samuel Rüegger <samuel@rueegger.me>
2020-05-25 22:10:13 +0200

    bugfix Google Map Darstellung

--- wp-content/themes/sanpool/templates/blocks/google-maps.php
+++ wp-content/themes/sanpool/templates/blocks/google-maps.php
@@ -4,17 +4,8 @@ $id = 'google-maps-' . $block['id'];
 if( !empty($block['anchor']) ) {
 	$id = $block['anchor'];
 }
-
-// Create class attribute allowing for custom "className" and "align" values.
-$className = 'google-maps-block';
-if( !empty($block['className']) ) {
-	$className .= ' ' . $block['className'];
-}
-if( !empty($block['align']) ) {
-	$className .= ' align' . $block['align'];
-}
 ?>
-<div id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $className ); ?>">
+<div id="<?php echo esc_attr( $id ); ?>">
 	<?php
 	if(have_rows( 'block_gm_locations' )) {
 		echo '<div class="sanpool-map" data-zoom="14">';
