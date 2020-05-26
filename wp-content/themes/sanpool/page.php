2220665bb42d4afdbf209b8e60020abacef0915d
Samuel Rüegger <samuel@rueegger.me>
2020-05-26 07:52:21 +0200

    Textseiten volle Breite

--- wp-content/themes/sanpool/page.php
+++ wp-content/themes/sanpool/page.php
@@ -4,20 +4,20 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
 <main id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="container">
-		<div class="row justify-content-center">
+		<div class="row">
 			<?php
 			$page_body_top_margin = '';
 			if(!get_field('page_has_title')) {
 				/* Titel auf der Seite wird angezeigt */
 				$page_body_top_margin = ' mt-5';
 				?>
-				<div class="col-12 col-lg-10 mt-4">
+				<div class="col-12 mt-4">
 					<?php the_title('<h1>', '</h1>'); ?>
 				</div>
 				<?php
 			}
 			?>
-			<div class="col-12 col-lg-10<?php echo $page_body_top_margin; ?>">
+			<div class="col-12<?php echo $page_body_top_margin; ?>">
 				<?php the_content(); ?>
 			</div>
 		</div>
